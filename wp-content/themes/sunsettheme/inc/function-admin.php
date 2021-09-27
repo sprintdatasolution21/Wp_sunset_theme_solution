1d839b9dabebbe9654b27ef8599ae2fbc62cf585
harsh paliwal <paliwal_harshit@yahoo.co.in>
2021-09-27 19:30:38 +0530

    Lession-04-1

--- wp-content/themes/sunsettheme/inc/function-admin.php
+++ wp-content/themes/sunsettheme/inc/function-admin.php
@@ -8,45 +8,66 @@
 		ADMIN PAGE
 	========================
 */
-function sunset_custom_settings() {
-	register_setting( 'sunset-settings-group', 'first_name' );
-	register_setting( 'sunset-settings-group', 'last_name' );
-		register_setting( 'sunset-settings-group', 'twitter_handler' );
-	add_settings_section( 'sunset-sidebar-options', 'Sunset Sidebar Option', 'sunset_sidebar_options', 'alecaddd_sunset');	
-
-	add_settings_field( 'sidebar-first-name', ' First Name',    'sunset_sidebar_first_name',      'alecaddd_sunset', 'sunset-sidebar-options');
-	add_settings_field( 'sidebar-last-name','Last Name',    'sunset_sidebar_last_name',      'alecaddd_sunset', 'sunset-sidebar-options');
-	add_settings_field( 'sidebar-twittter', 'Twitter Handler',    'sunset_sidebar_twittter',      'alecaddd_sunset', 'sunset-sidebar-options');
-
-}
-
-
-
-function sunset_sidebar_options() {
-	echo 'Customize your Sidebar Information Below >>';
-}
-function sunset_sidebar_first_name() {
-	$firstName = esc_attr( get_option( 'first_name' ) );
-	echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="first Name" />';
-}
-function sunset_sidebar_last_name() {
-	$lastName = esc_attr( get_option( 'last_name' ) );
-	echo '<input type="text" name="last_name" value="'.$lastName.'" placeholder="Last Name" />';
-}
-function sunset_sidebar_twittter() {
-	$twitterhandler = esc_attr( get_option( 'twitter_handler' ) );
-	echo '<input type="text" name="twitter_handler" value="'.$twitterhandler.'" placeholder="Twitter Handler" />';
-}
-
 
 function sunset_add_admin_page() {
 	
 	//Generate Sunset Admin Page
 	add_menu_page( 'Sunset Theme Options', 'Sunset', 'manage_options', 'alecaddd_sunset', 'sunset_theme_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110 );
-		//Generate Sunset Admin Sub Pag  s
+	
+	//Generate Sunset Admin Sub Pages
 	add_submenu_page( 'alecaddd_sunset', 'Sunset Theme Options', 'General', 'manage_options', 'alecaddd_sunset', 'sunset_theme_create_page' );
 	add_submenu_page( 'alecaddd_sunset', 'Sunset CSS Options', 'Custom CSS', 'manage_options', 'alecaddd_sunset_css', 'sunset_theme_settings_page');
 	
+	
+	
+}
+add_action( 'admin_menu', 'sunset_add_admin_page' );
+
+//Activate custom settings
+	add_action( 'admin_init', 'sunset_custom_settings' );
+
+function sunset_custom_settings() {
+	register_setting( 'sunset-settings-group', 'first_name' );
+	register_setting( 'sunset-settings-group', 'last_name' );
+	register_setting( 'sunset-settings-group', 'twitter_handler', 'sunset_sanitize_twitter_handler' );
+	register_setting( 'sunset-settings-group', 'facebook_handler' );
+	register_setting( 'sunset-settings-group', 'gplus_handler' );
+	
+	add_settings_section( 'sunset-sidebar-options', 'Sidebar Option', 'sunset_sidebar_options', 'alecaddd_sunset');
+	
+	add_settings_field( 'sidebar-name', 'Full Name', 'sunset_sidebar_name', 'alecaddd_sunset', 'sunset-sidebar-options');
+	add_settings_field( 'sidebar-twitter', 'Twitter handler', 'sunset_sidebar_twitter', 'alecaddd_sunset', 'sunset-sidebar-options');
+	add_settings_field( 'sidebar-facebook', 'Facebook handler', 'sunset_sidebar_facebook', 'alecaddd_sunset', 'sunset-sidebar-options');
+	add_settings_field( 'sidebar-gplus', 'Google+ handler', 'sunset_sidebar_gplus', 'alecaddd_sunset', 'sunset-sidebar-options');
+}
+
+function sunset_sidebar_options() {
+	echo 'Customize your Sidebar Information';
+}
+
+function sunset_sidebar_name() {
+	$firstName = esc_attr( get_option( 'first_name' ) );
+	$lastName = esc_attr( get_option( 'last_name' ) );
+	echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="First Name" /> <input type="text" name="last_name" value="'.$lastName.'" placeholder="Last Name" />';
+}
+function sunset_sidebar_twitter() {
+	$twitter = esc_attr( get_option( 'twitter_handler' ) );
+	echo '<input type="text" name="twitter_handler" value="'.$twitter.'" placeholder="Twitter handler" /><p class="description">Input your Twitter username without the @ character.</p>';
+}
+function sunset_sidebar_facebook() {
+	$facebook = esc_attr( get_option( 'facebook_handler' ) );
+	echo '<input type="text" name="facebook_handler" value="'.$facebook.'" placeholder="Facebook handler" />';
+}
+function sunset_sidebar_gplus() {
+	$gplus = esc_attr( get_option( 'gplus_handler' ) );
+	echo '<input type="text" name="gplus_handler" value="'.$gplus.'" placeholder="Google+ handler" />';
+}
+
+//Sanitization settings
+function sunset_sanitize_twitter_handler( $input ){
+	$output = sanitize_text_field( $input );
+	$output = str_replace('@', '', $output);
+	return $output;
 }
 
 function sunset_theme_create_page() {
@@ -54,10 +75,7 @@ function sunset_theme_create_page() {
 }
 
 function sunset_theme_settings_page() {
-	require_once(get_template_directory().'/inc/templates/sunset-settings.php');		
+	
+	echo '<h1>Sunset Custom CSS</h1>';
+	
 }
-
-//Hoocks
-add_action( 'admin_menu', 'sunset_add_admin_page' );
-//Activate custom settings
-add_action( 'admin_init', 'sunset_custom_settings' );
